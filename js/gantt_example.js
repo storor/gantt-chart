3a4c0a5a851c332daaa6d031bae3e66db199a8b4
v-stolok <sergiy.tolok@hotmail.com>
2014-10-08 19:33:44 +0300

    Enables switching between proportial and non prop.  modes

--- js/gantt_example.js
+++ js/gantt_example.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
+	var slas = {
+	    AIR_TCKT_TRANS_FACT : '7:30',
+	    BUSINESS_PARTNR_DIM : '7:30',
+	    CAR_RENTL_TRANS_FACT : '7:30',
+	    CRUIS_CABN_TRANS_FACT : '7:30',
+	    DEST_SVC_TCKT_TRANS_FACT : '7:30',
+	    INS_ITM_TRANS_FACT : '7:30',
+	    LODG_RM_NIGHT_TRANS_FACT : '7:30',
+	    MGMT_UNIT_DIM : '4:00',
+	    PKG_TRANS_FACT : '7:30',
+	    TRAIN_BKG_TRANS_FACT : '7:30',
+	    NOT_AIR_TCKT_TRANS_FACT : '6:00'
+
+	}
      var parseData = function(rawData){
         if(!rawData){
             return [];
@@ -8,7 +22,21 @@ $( document ).ready(function() {
 			var group = element.TabName;
 			groups[group] = groups[group] || {};
 			groups[group].name = element.TabName;
+			groups[group].sla = slas[element.TabName];
 			groups[group].series = groups[group].series || [];
+			
+			var slaDate = new Date(element.RunStart);
+			slaDate.setHours(0);
+			slaDate.setMinutes(0);
+			slaDate.setSeconds(0);
+			slaDate.setMilliseconds(0);
+			groups[group].slas = groups[group].slas || [];
+			
+			var slaTime = slaDate.clone();
+			slaTime.setHours(slas[element.TabName].split(':')[0]);
+            slaTime.setMinutes(slas[element.TabName].split(':')[1]);
+			groups[group].slas[slaDate.getTime()] = slaTime;
+
 			groups[group].series[groups[group].series.length] = {
 				name: element.job,
 				status: element.Status,
@@ -18,22 +46,30 @@ $( document ).ready(function() {
 		});
 		var array = $.map(groups, function(value, index) {
 		    return [value];
-		});
+		});		
 		return array;
 	}
-	var parsedData = parseData(data);
+	window.parsedData = parseData(data);
+
 	if(parsedData.length == 0){
 	    return;
 	}
    $('.content').ganttView({ 
 		data: parsedData,
 		slideWidth: $('.content').width()-280,
-		cellWidth: 61,
+		cellWidth: 101,
 		colors: {
 			Success: '#DFF2BF',
 			Running: '#BDE5F8',
-			Invalid: '#D8000C',
+			Invalid: '#FC5059',
 			Failed: '#FFBABA'
-		}
-	});	
+		},
+		link: ' http://chc-utldwh01/cgi-bin/ctmdshbrd.pl?search=',
+        link_title:  'Show Jobs in Contor-M'
+	});
+
+	$('.ganttview-statuss-info').on('click',function(evt){
+		console.log(evt);
+		console.log(this);
+	})	
 });
--- js/lib/jQuery.ganttView.js
+++ js/lib/jQuery.ganttView.js
@@ -1,7 +1,5 @@
 /*
 Based on jQuery.ganttView v.0.8.8
-Copyright (c) 2010 JC Grubbs - jc.grubbs@devmynd.com
-MIT License Applies
 */
 (function (jQuery) {
     
@@ -27,7 +25,8 @@ MIT License Applies
             cellHeight: 31,
             slideWidth: 500,
             vHeaderWidth: 100,
-            cellMinWidth: 31
+            cellMinWidth: 91,
+            showEmpty: false
         };
         
         var opts = jQuery.extend(true, defaults, options);
@@ -45,17 +44,16 @@ MIT License Applies
             opts.end = startEnd[1];
             
             els.each(function () {
-
                 var container = jQuery(this);
                 var div = jQuery("<div>", { "class": "ganttview" });
                 var chart = new Chart(div, opts);
                 chart.render();
-                container.append(div);
+                container.append(div);                
                 
-                var w = jQuery("div.ganttview-vtheader", container).outerWidth() +
-                    jQuery("div.ganttview-slide-container", container).outerWidth();
-                container.css("width", (w + 2) + "px");
-                handleScroll(jQuery("div.ganttview-slide-container", container),els,opts);
+                var w = jQuery("div.ganttview-vtheader", container).width() +
+                    jQuery("div.ganttview-slide-container", container).width();
+                $('div.ganttview',container).css("width", (w + 3) + "px");
+                handleScroll(jQuery("div.ganttview-slide-container", container),els,opts);                
             });
         }
     }
@@ -78,7 +76,6 @@ MIT License Applies
 
     function rerender(increase,els,opts){
         var newWidth = opts.cellWidth + (increase?10:-10);
-        console.log((opts.cellMinWidth > newWidth) + ' '+ opts.cellMinWidth + ' '+ newWidth);
         if(opts.cellMinWidth >= newWidth){ return;}
         opts.cellWidth = newWidth;
         $(els).empty();
@@ -100,19 +97,29 @@ MIT License Applies
     var Chart = function(div, opts) {
         
         function render() {
-            addVtHeader(div, opts.data, opts.cellHeight);
+            addVtHeader(div, opts.data, opts.cellHeight,opts.link,opts.link_title);
 
             var slideDiv = jQuery("<div>", {
                 "class": "ganttview-slide-container",
                 "css": { "width": opts.slideWidth + "px" }
             });
-            
-            dates = getDatesWithHours(opts.start, opts.end);
+            var last = {};
+            var first = new Date(opts.start.getFullYear(),opts.start.getMonth(),opts.start.getDate(),opts.start.getHours());
+            dates = getDatesWithHours(first, opts.end, last, opts.data, opts.showEmpty);
+
+            var cellWidthCalculated = Math.round((opts.slideWidth / (DateUtils.minutesBetween(first, last.value)-
+                (opts.showEmpty ? 0 
+                        : DateUtils.emptyMinutesCompensation(dates,first, last.value))))*60);
+
+            opts.cellWidth = opts.cellMinWidth > cellWidthCalculated ? opts.cellMinWidth : cellWidthCalculated;
+
             addHzHeaderWithHours(slideDiv, dates, opts.cellWidth);
             addGrid(slideDiv, opts.data, dates, opts.cellWidth, opts.showWeekends);
             addBlockContainers(slideDiv, opts.data);
-            addBlocksWithHours(slideDiv, opts.data, opts.cellWidth, opts.start);
+            addBlocksWithHours(slideDiv, opts.data, opts.cellWidth, first, last.value,dates, opts.showEmpty);
             div.append(slideDiv);
+            
+            
             applyLastClass(div.parent());
         }
         
@@ -121,8 +128,7 @@ MIT License Applies
         
         // Creates a 3 dimensional array [year][month][day][hour] of every hour 
         // between the given start and end dates
-        function getDatesWithHours(start, end) {
-            window.start = start;
+        function getDatesWithHours(start, end, lastest,data, showEmpty) {
             var dates = [];
             dates[start.getFullYear()] = [];
             dates[start.getFullYear()][start.getMonth()] = [];
@@ -138,9 +144,15 @@ MIT License Applies
                 if (!dates[next.getFullYear()][next.getMonth()][next.getDate()]) { 
                     dates[next.getFullYear()][next.getMonth()][next.getDate()] = []; 
                 }
-                dates[next.getFullYear()][next.getMonth()][next.getDate()].push(next);
+                if(showEmpty || !DateUtils.isEmpty(data, next)){
+                    dates[next.getFullYear()][next.getMonth()][next.getDate()].push(next);
+                }
                 last = next;
             }
+            lastest.value = last.clone().addMinutes(60 -last.getMinutes());
+            lastest.value.setSeconds(0);
+            lastest.value.setMilliseconds(0);
+            console.log(lastest.value);
             return dates;
         }
 
@@ -159,11 +171,11 @@ MIT License Applies
                         for (var h in dates[y][m][d]) {
                             hoursDiv.append(jQuery("<div>", { 
                                 "class": "ganttview-hzheader-hour",
-                                "css": { "width": cellWidth  - 1 + "px" }                              
+                                "css": { "width": cellWidth  - 2 + "px" }                              
                              }).append(dates[y][m][d][h].getHours()+':00'));
                         }
                         var size = realSize(dates[y][m][d]);
-                        var w = size * cellWidth;
+                        var w = size * (cellWidth-1);
                         totalW = totalW + w;
                         daysDiv.append(jQuery("<div>", {
                             "class": "ganttview-hzheader-day",
@@ -178,7 +190,7 @@ MIT License Applies
             div.append(headerDiv);
         }
 
-        function addVtHeader(div, data, cellHeight) {
+        function addVtHeader(div, data, cellHeight,link,title) {
             var headerDiv = jQuery("<div>", { "class": "ganttview-vtheader" });
             var headerInfoDiv = jQuery("<div>", { "class": "ganttview-vtheader-info" });
             headerInfoDiv.append(jQuery("<div>", { "class": "ganttview-vtheader-info-tables" }));
@@ -191,9 +203,43 @@ MIT License Applies
                 itemDiv.append(jQuery("<div>", {
                     "class": "ganttview-vtheader-item-name",
                     "css": { "height": ( cellHeight) + "px" }
-                }).append(data[i].name));
+                }).append(jQuery("<a>", {
+                    name : "link",
+                    target:"_blank",
+                    href : link+data[i].name,
+                    text : data[i].name,
+                    title : title
+                })));
+                
                 headerDiv.append(itemDiv);
             }
+            var statusess = jQuery("<div>", {
+                "class": "ganttview-statusses-info"                
+            });
+            /*for(var i in opts.colors){
+                statusess.append(jQuery("<div>", {
+                "class": "ganttview-statuss-info",
+                "css":{"background-color": opts.colors[i]}
+                }).append(i));
+
+            }*/
+            var that = this;
+            statusess.append(jQuery('<input>',{
+                type: 'checkbox',
+                id: 'show-empty-chbx',
+                change: function(){
+                    opts.showEmpty = this.checked;
+                    $(div).empty();
+                    render();
+                    console.log('Changed');
+                }
+            }).attr('checked',opts.showEmpty)).append(jQuery('<lable>',{
+                text: 'show',
+                 'class': 'show-empty-chbx-lbl',
+                'for': 'show-empty-chbx',
+
+            }));
+            headerDiv.append(statusess);
             div.append(headerDiv);
         }        
 
@@ -206,13 +252,13 @@ MIT License Applies
                         for (var d in dates[y][m][d]) {
                             var cellDiv = jQuery("<div>", { 
                                 "class": "ganttview-grid-row-cell",
-                                 "css": { "width": cellWidth  - 1 + "px" }});
+                                 "css": { "width": cellWidth  - 2 + "px" }});
                             rowDiv.append(cellDiv);
                         }                       
                     }
                 }
             }
-            var w = jQuery("div.ganttview-grid-row-cell", rowDiv).length * cellWidth;
+            var w = jQuery("div.ganttview-grid-row-cell", rowDiv).length * (cellWidth-1);
             rowDiv.css("width", w + "px");
             gridDiv.css("width", w + "px");
             for (var i = 0; i < data.length; i++) {
@@ -229,35 +275,71 @@ MIT License Applies
             div.append(blocksDiv);
         }
 
-        function addBlocksWithHours(div, data, cellWidth, start) {
+        function addBlocksWithHours(div, data, cellWidth, start, maxDate,dates, showEmpty) {
             var rows = jQuery("div.ganttview-blocks div.ganttview-block-container", div);
             var rowIdx = 0;
-            var minuetsRatio =  ( cellWidth )/60.0;
+            var minuetsRatio =  ( cellWidth-1 )/60.0;
             for (var i = 0; i < data.length; i++) {
+                var slaHours = data[i].sla.split(':')[0];
+                var slaMinutes = data[i].sla.split(':')[1];
+                //date for which SLA is already rendered
+                var lastSLADate = undefined;
                 for (var j = 0; j < data[i].series.length; j++) {
                     var series = data[i].series[j];
-
-                    var sizeMinuets =  DateUtils.minutesBetween(series.start, series.end);
-                    var offsetMinuets = DateUtils.minutesBetween(start, series.start);
-
-                    //console.log(offsetMinuets*minuetsRatio+' where offset: '+offsetMinuets+' minuetsRatio: '+minuetsRatio);
+                    
+                    var sizeMinuets =  DateUtils.minutesBetween(series.start, series.end || maxDate) ;
+                    var offsetMinuets = DateUtils.minutesBetween(start, series.start) - 
+                        (showEmpty ? 0 
+                        : DateUtils.emptyMinutesCompensation(dates,series.start, start));
+                    
+                    var offset =  Math.round(offsetMinuets*minuetsRatio) ;
                     var size = Math.round(sizeMinuets*minuetsRatio);
-                    var offset = Math.round(offsetMinuets*minuetsRatio);
-
+                
                     var block = jQuery("<div>", {
                         "class": "ganttview-block",
-                        "title": series.name + ", status : " +series.status + ', time spent :' + (size == 0 ? 'less that 1 minute' : size + " minutes")+ ' , Started at : '+series.start,
+                        "title": series.name + ", status : " +series.status + ', time spent: ' + (series.end ? (size == 0 ? 'less that 1 minute' : Math.round(sizeMinuets) + " minutes") : 'still running')+ '\nStarted at : '+series.start.toString("HH:mm:ss")+ (series.end?', ended at ' +series.end.toString("HH:mm:ss"):''),
                         "css": {
-                            "width": (size===0?1:size) + "px",
-                            "margin-left": offset +5+ "px"
+                            "width": (size===0?1:size - 1) + "px",
+                            "margin-left": offset + "px"
                         }
                     });
+                    
                     addBlockData(block, data[i], series);
+
+                    //Set color of the job
                     if (opts.colors) {
                         block.css("background-color", opts.colors[data[i].series[j].status]);
                     }
-                    //block.append(jQuery("<div>", { "class": "ganttview-block-text" }).text(size == 0 ? '' : Math.round(sizeMinuets)));
-                    jQuery(rows[rowIdx]).append(block);                    
+
+                    jQuery(rows[rowIdx]).append(block); 
+
+                    if(lastSLADate === undefined || (lastSLADate.getDate() !== series.start.getDate()||
+                        lastSLADate.getMonth() !== series.start.getMonth() || 
+                        lastSLADate.getYear() !== series.start.getYear())){                        
+                        
+                        var sla = series.start.clone();
+                        sla.setHours(slaHours);
+                        sla.setMinutes(slaMinutes);
+                        sla.setSeconds(0);
+                        if(maxDate.isAfter(sla)){
+                            var slaOffsetMinuets = DateUtils.minutesBetween(start, sla) - 
+                            (showEmpty ? 0 
+                            : DateUtils.emptyMinutesCompensation(dates,sla, start));;
+                            
+                            var slaOffset =  Math.round(slaOffsetMinuets*minuetsRatio) ;
+        
+                            var slaBlock = jQuery("<div>", {
+                                "class": "ganttview-block-sla",
+                                "title": 'SLA is ' + data[i].sla,
+                                "css": {
+                                    "width": "2px",
+                                    "margin-left": slaOffset + "px"
+                                }
+                            });
+                            jQuery(rows[rowIdx]).append(slaBlock); 
+                        }
+                        lastSLADate = series.start.clone();
+                    }
                 }
                 rowIdx = rowIdx + 1;
             }
@@ -281,65 +363,70 @@ MIT License Applies
         return {
             render: render
         };
-    }
-
-    var ArrayUtils = {
-    
-        contains: function (arr, obj) {
-            var has = false;
-            for (var i = 0; i < arr.length; i++) { if (arr[i] == obj) { has = true; } }
-            return has;
-        }
-    };
+    }    
 
     var DateUtils = {
         
-        hoursBetween: function (start, end) {
-            if (!start || !end) { return 0; }
-            start = Date.parse(start); end = Date.parse(end);
-            if (start.getYear() == 1901 || end.getYear() == 8099) { return 0; }
-            var count = 0, date = start.clone();
-            while (date.compareTo(end) == -1) { count = count + 1; date.addDays(1); }
-            return count;
+        emptyMinutesCompensation: function(dates, date, start){
+            var hours = -1;
+            for (var y in dates) {
+                for (var m in dates[y]) {
+                    for (var d in dates[y][m]) {
+                        for (var h in dates[y][m][d]) {
+                            if(dates[y][m][d][h].isBefore(date) ||  dates[y][m][d][h].equals(date)){
+                                hours++;
+                            }
+                        }
+                    }
+                }
+            }
+            var dateWithoutMinutes = date.clone();
+            dateWithoutMinutes.setMinutes(0);
+            dateWithoutMinutes.setSeconds(0);
+            dateWithoutMinutes.setMilliseconds(0);
+            return (dateWithoutMinutes.getTime() - start.getTime())/(1000*60)- hours*60;
         },
-        
+
+        isEmpty: function(data, date){
+            var nextHour = date.clone();
+            nextHour.addHours(1);
+            for (var i = data.length - 1; i >= 0; i--) {
+                var slaDate = date.clone();
+                slaDate.setHours(0);
+                slaDate.setMinutes(0);
+                slaDate.setSeconds(0);
+                slaDate.setMilliseconds(0);  
+                var time = slaDate.getTime();              
+                if(data[i].slas[time] && data[i].slas[time].between(date,nextHour)){
+                    return false;
+                }
+                for (var j = 0; j < data[i].series.length; j++) {
+                    if(data[i].series[j].start.between(date,nextHour)||
+                        data[i].series[j].end.between(date,nextHour) || 
+                        (data[i].series[j].start.isAfter(date) && 
+                            data[i].series[j].end.isBefore(nextHour))){
+                        return false;
+                    }
+                };
+            };
+            return true;
+        },
+
         minutesBetween: function (start, end) {
             if (!start || !end) { return 0; }
             start = Date.parse(start); end = Date.parse(end);
             if (start.getYear() == 1901 || end.getYear() == 8099) { return 0; }
             var result = (end.getTime() - start.getTime())/(1000*60);//in minutes
             return result;
-        },
-
-        isWeekend: function (date) {
-            return date.getDay() % 6 == 0;
-        },
-
-        getBoundaryDatesFromData: function (data, minDays) {
-            var minStart = new Date(); maxEnd = new Date();
-            for (var i = 0; i < data.length; i++) {
-                for (var j = 0; j < data[i].series.length; j++) {
-                    var start = Date.parse(data[i].series[j].start);
-                    var end = Date.parse(data[i].series[j].end)
-                    if (i == 0 && j == 0) { minStart = start; maxEnd = end; }
-                    if (minStart.compareTo(start) == 1) { minStart = start; }
-                    if (maxEnd.compareTo(end) == -1) { maxEnd = end; }
-                }
-            }
-            
-            // Insure that the width of the chart is at least the slide width to avoid empty
-            // whitespace to the right of the grid
-            if (DateUtils.daysBetween(minStart, maxEnd) < minDays) {
-                maxEnd = minStart.clone().addDays(minDays);
-            }
-            
-            return [minStart, maxEnd];
-        },
+        },        
 
         getBoundaryDatesFromDataWithHours: function (data) {
             var minStart = new Date(); maxEnd = new Date();
             for (var i = 0; i < data.length; i++) {
                 for (var j = 0; j < data[i].series.length; j++) {
+                    if(!data[i].series[j].end || !data[i].series[j].start){
+                        continue;
+                    }
                     var start = Date.parse(data[i].series[j].start);
                     var end = Date.parse(data[i].series[j].end)
                     if (i == 0 && j == 0) { minStart = start; maxEnd = end; }
